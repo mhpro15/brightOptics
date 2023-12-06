9aa661634e61ee41b37458695ac335b6772784ec
Hung Nguyen <nguyenmanhhungpro15@gmail.com>
2023-12-05 22:24:16 -0500

    first commit

---
+++ core/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+import './css/mystyle.css'
+
+
+export default function Home(){ 
+// const classes = useStyles()
+return (
+    
+
+  <div className= "container1">
+     <img src="./images/slider-bg.jpg" alt="glasses" ></img>
+     <div className="top-left">  Bright Optics!</div>
+     <div className = "button" > <a href="/shop"><button className = "button1" type="button">Buy Now</button></a></div>
+
+  </div>
+
+ 
+)}
+
+
+
